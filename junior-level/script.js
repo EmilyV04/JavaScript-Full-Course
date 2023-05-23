4b6d547dd1e26626387f2eac2516762571240e61
EmilyV04 <emily.vanessavilla@hotmail.com>
2023-05-23 15:39:56 -0500

    First commit level junior Variable types and prompts

---
+++ junior-level/script.js
@@ -0,0 +1,12 @@
+// TIPOS DE VARIABLES
+string = "asdfghj";
+number = 123456;
+boolean = true;
+
+// var -> variable global
+// let -> variable local
+// const -> constante (no cambia su valor)
+
+// PROMPT
+let name = prompt("cual es tu nombre?");
+alert("hola " + name);
