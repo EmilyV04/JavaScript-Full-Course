89ff69d18636d7b65b2587775b39f10dceb7af20
EmilyV04 <emily.vanessavilla@hotmail.com>
2023-05-23 15:58:26 -0500

    Arithmetics and assignment operators

--- junior-level/script.js
+++ junior-level/script.js
@@ -1,12 +1,33 @@
 // TIPOS DE VARIABLES
-string = "asdfghj";
-number = 123456;
-boolean = true;
+// string = "asdfghj";
+// number = 123456;
+// boolean = true;
 
 // var -> variable global
 // let -> variable local
 // const -> constante (no cambia su valor)
 
 // PROMPT
-let name = prompt("cual es tu nombre?");
-alert("hola " + name);
+// let name = prompt("cual es tu nombre?");
+// alert("hola " + name);
+
+//OPERADORES DE ASIGNACIÓN
+x += y;
+x -= y;
+x *= y;
+x /= y;
+x %= y;
+x **= y;
+x--;
+x++;
+
+//OPERADORES ARITMÉTICOS
+x + y;
+x - y;
+x * y;
+x / y;
+x % y;
+x ** y;
+
+
+
